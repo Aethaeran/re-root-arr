4e29da842c07fc8889a0bdc6a5d48ce5933cff23
Aethaeran <33813964+Aethaeran@users.noreply.github.com>
2020-06-23 16:35:40 -0700

    Update re-root.py
    Ended up splitting it into two separate functions. Added some notes about additional functionality that could be added regarding switching between Windows and Linux. Also have a couple lines commented out that can be used to replace their following lines that would allow you to set all NetImport in the Radarr database at once. Works as it is, so will probably shelve this project until I need it next.

--- re-root.py
+++ re-root.py
@@ -3,7 +3,6 @@
 import os
 import re
 import sqlite3
-import pandas as pd
 
 # Settings are always nice
 
@@ -18,18 +17,114 @@ radarr_root_directory_to_replace_with = "/your/new/root"
 # Define functions here
 
 
-def change_root_directories(your_database, replace_this, with_this):
-    # Incomplete function
+def sonarr_change_root_directories(your_database, replace_this, with_this):
+    '''
+
+    :param your_database:
+    :param replace_this:
+    :param with_this:
+    :return:
+    '''
+
     if os.path.exists(your_database):
-        current_string = ""
-        new_string = re.sub(replace_this, with_this, current_string)
-    return
+        conn = sqlite3.connect(your_database)
+        c = conn.cursor()
+
+        # Sonarr DB tables that need updating
+        # RootFolders, Path column
+        root_folders_to_change = []
+        for row in c.execute('SELECT * FROM RootFolders WHERE Path Like "' + str(replace_this) + '%"'):
+            root_folders_to_change.append(row[1])  # Append Path column to list
+
+        for item in root_folders_to_change:
+            print(item)
+            new_path = re.sub(replace_this, with_this, item)
+            c.execute('UPDATE RootFolders SET Path="' + new_path + '" WHERE Path like "' + item + '%"')
+
+        series_to_change = []
+        for row in c.execute('SELECT * FROM Series WHERE Path Like "' + str(replace_this) + '%"'):
+            series_to_change.append(row[11])  # Append Path column to list
+
+        for item in series_to_change:
+            print(item)
+            new_path = re.sub(replace_this, with_this, item)
+            c.execute('UPDATE Series SET Path="' + new_path + '" WHERE Path like "' + item + '%"')
+
+        # You'd want to change these too if you were making the change between Windows and Linux
+
+        # MetadataFiles, RelativePath
+        # SubtitleFiles, RelativePath
+        # ExtraFiles, RelativePath
+        # EpisodeFiles, RelativePath
+
+        # Save (commit) the changes
+        conn.commit()
+        conn.close()
+
+
+def radarr_change_root_directories(your_database, replace_this, with_this):
+    '''
+
+    :param your_database:
+    :param replace_this:
+    :param with_this:
+    :return:
+    '''
+
+    if os.path.exists(your_database):
+        conn = sqlite3.connect(your_database)
+        c = conn.cursor()
+
+        # Radarr DB tables that need updating
+        # RootFolders, Path column
+        root_folders_to_change = []
+        for row in c.execute('SELECT * FROM RootFolders WHERE Path Like "' + str(replace_this) + '%"'):
+            root_folders_to_change.append(row[1])  # Append Path column to list
+
+        for item in root_folders_to_change:
+            print(item)
+            new_path = re.sub(replace_this, with_this, item)
+            c.execute('UPDATE RootFolders SET Path="' + new_path + '" WHERE Path like "' + item + '%"')
+
+        # Movies, Path column
+        movies_to_change = []
+        for row in c.execute('SELECT * FROM Movies WHERE Path Like "' + str(replace_this) + '%"'):
+            movies_to_change.append(row[9])  # Append Path column to list
+
+        for item in movies_to_change:
+            print(item)
+            new_path = re.sub(replace_this, with_this, item)
+            c.execute('UPDATE Movies SET Path="' + new_path + '" WHERE Path like "' + item + '%"')
+
+        # NetImport, RootFolderPath column
+        net_import_to_change = []
+        # If you uncomment the following line, and comment the one following that,
+        # and do that in the following block as well,
+        # then you can use these two blocks to set the RootFolderPath on all NetImports simultaneously
+        # for row in c.execute('SELECT * FROM NetImport WHERE RootFolderPath Like "%"'):
+        for row in c.execute('SELECT * FROM NetImport WHERE RootFolderPath Like "' + str(replace_this) + '%"'):
+            net_import_to_change.append(row[7])  # Append RootFolderPath column to list
+
+        for item in net_import_to_change:
+            print(item)
+            new_path = re.sub(replace_this, with_this, item)
+            # c.execute('UPDATE NetImport SET RootFolderPath="' + with_this + '" WHERE RootFolderPath like "%"')
+            c.execute('UPDATE NetImport SET RootFolderPath="'+new_path+'" WHERE RootFolderPath like "'+item+'%"')
+
+        # You'd want to change these too if you were making the change between Windows and Linux
+
+        # SubtitleFiles, RelativePath
+        # MovieFiles, RelativePath column # Probably unnecessary. I don't think Radarr supports sub-folders.
+
+        # Save (commit) the changes
+        conn.commit()
+        conn.close()
 
 
 # Main Script
 
 # Apply to sonarr
-change_root_directories(sonarr_db, sonarr_root_directory_to_replace, sonarr_root_directory_to_replace_with)
+sonarr_change_root_directories(sonarr_db, sonarr_root_directory_to_replace, sonarr_root_directory_to_replace_with)
 
 # Apply to radarr
-change_root_directories(radarr_db, radarr_root_directory_to_replace, radarr_root_directory_to_replace_with)
+radarr_change_root_directories(radarr_db, radarr_root_directory_to_replace, radarr_root_directory_to_replace_with)
