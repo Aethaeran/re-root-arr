21e3d5cd5e07cb159228b90bbf2138f2d6374306
Aethaeran <33813964+Aethaeran@users.noreply.github.com>
2021-07-13 14:31:07 -0700

    Update re-root.py
    Some changes to make it work with current versions of Radarr.
    NetImport was changed to ImportLists in the Radarr database, and the default name for the database is now "radarr.db"
    Removed some unnecessary comments.
    Converted docstrings to
    the triple double-quoted string form.
    Changed printout to show what string it is changing, AND what it is changing to.

--- re-root.py
+++ re-root.py
@@ -1,30 +1,27 @@
-# Naturally we need to import necessary libraries. You'll need to install pandas if you haven't already.
-
 import os
 import re
 import sqlite3
 
-# Settings are always nice
+# Here is where you customize settings.
 
-sonarr_db = "sonarr.db"  # You direct the script to your Sonarr database here, make sure it isn't running at the time.
+# You direct the script to your Sonarr database here, make sure it isn't running at the time.
+sonarr_db = "sonarr.db"
 sonarr_root_directory_to_replace = "/your/current/root"
-sonarr_root_directory_to_replace_with = "/your/new/root"
+sonarr_root_directory_replacement = "/your/new/root"
 
-radarr_db = "nzbdrone.db"  # You direct the script to your Radarr database here, make sure it isn't running at the time.
+# You direct the script to your Radarr database here, make sure it isn't running at the time.
+radarr_db = "radarr.db"
 radarr_root_directory_to_replace = "/your/current/root"
-radarr_root_directory_to_replace_with = "/your/new/root"
-
-# Define functions here
+radarr_root_directory_replacement = "/your/new/root"
 
 
 def sonarr_change_root_directories(your_database, replace_this, with_this):
-    '''
-
+    """
     :param your_database:
     :param replace_this:
     :param with_this:
     :return:
-    '''
+    """
 
     if os.path.exists(your_database):
         conn = sqlite3.connect(your_database)
@@ -37,8 +34,8 @@ def sonarr_change_root_directories(your_database, replace_this, with_this):
             root_folders_to_change.append(row[1])  # Append Path column to list
 
         for item in root_folders_to_change:
-            print(item)
             new_path = re.sub(replace_this, with_this, item)
+            print(item+" --> "+new_path)
             c.execute('UPDATE RootFolders SET Path="' + new_path + '" WHERE Path like "' + item + '%"')
 
         series_to_change = []
@@ -46,8 +43,8 @@ def sonarr_change_root_directories(your_database, replace_this, with_this):
             series_to_change.append(row[11])  # Append Path column to list
 
         for item in series_to_change:
-            print(item)
             new_path = re.sub(replace_this, with_this, item)
+            print(item+" --> "+new_path)
             c.execute('UPDATE Series SET Path="' + new_path + '" WHERE Path like "' + item + '%"')
 
         # You'd want to change these too if you were making the change between Windows and Linux
@@ -64,7 +61,6 @@ def sonarr_change_root_directories(your_database, replace_this, with_this):
 
 def radarr_change_root_directories(your_database, replace_this, with_this):
     '''
-
     :param your_database:
     :param replace_this:
     :param with_this:
@@ -82,8 +78,8 @@ def radarr_change_root_directories(your_database, replace_this, with_this):
             root_folders_to_change.append(row[1])  # Append Path column to list
 
         for item in root_folders_to_change:
-            print(item)
             new_path = re.sub(replace_this, with_this, item)
+            print(item+" --> "+new_path)
             c.execute('UPDATE RootFolders SET Path="' + new_path + '" WHERE Path like "' + item + '%"')
 
         # Movies, Path column
@@ -92,24 +88,24 @@ def radarr_change_root_directories(your_database, replace_this, with_this):
             movies_to_change.append(row[9])  # Append Path column to list
 
         for item in movies_to_change:
-            print(item)
             new_path = re.sub(replace_this, with_this, item)
+            print(item+" --> "+new_path)
             c.execute('UPDATE Movies SET Path="' + new_path + '" WHERE Path like "' + item + '%"')
 
-        # NetImport, RootFolderPath column
+        # ImportLists, RootFolderPath column
         net_import_to_change = []
         # If you uncomment the following line, and comment the one following that,
         # and do that in the following block as well,
-        # then you can use these two blocks to set the RootFolderPath on all NetImports simultaneously
-        # for row in c.execute('SELECT * FROM NetImport WHERE RootFolderPath Like "%"'):
-        for row in c.execute('SELECT * FROM NetImport WHERE RootFolderPath Like "' + str(replace_this) + '%"'):
+        # then you can use these two blocks to set the RootFolderPath on all ImportListss simultaneously
+        # for row in c.execute('SELECT * FROM ImportLists WHERE RootFolderPath Like "%"'):
+        for row in c.execute('SELECT * FROM ImportLists WHERE RootFolderPath Like "' + str(replace_this) + '%"'):
             net_import_to_change.append(row[7])  # Append RootFolderPath column to list
 
         for item in net_import_to_change:
-            print(item)
             new_path = re.sub(replace_this, with_this, item)
-            # c.execute('UPDATE NetImport SET RootFolderPath="' + with_this + '" WHERE RootFolderPath like "%"')
-            c.execute('UPDATE NetImport SET RootFolderPath="'+new_path+'" WHERE RootFolderPath like "'+item+'%"')
+            print(item+" --> "+new_path)
+            # c.execute('UPDATE ImportLists SET RootFolderPath="' + with_this + '" WHERE RootFolderPath like "%"')
+            c.execute('UPDATE ImportLists SET RootFolderPath="'+new_path+'" WHERE RootFolderPath like "'+item+'%"')
 
         # You'd want to change these too if you were making the change between Windows and Linux
 
@@ -121,10 +117,8 @@ def radarr_change_root_directories(your_database, replace_this, with_this):
         conn.close()
 
 
-# Main Script
+# Apply changes to sonarr
+sonarr_change_root_directories(sonarr_db, sonarr_root_directory_to_replace, sonarr_root_directory_replacement)
 
-# Apply to sonarr
-sonarr_change_root_directories(sonarr_db, sonarr_root_directory_to_replace, sonarr_root_directory_to_replace_with)
-
-# Apply to radarr
-radarr_change_root_directories(radarr_db, radarr_root_directory_to_replace, radarr_root_directory_to_replace_with)
+# Apply changes to radarr
+radarr_change_root_directories(radarr_db, radarr_root_directory_to_replace, radarr_root_directory_replacement)
