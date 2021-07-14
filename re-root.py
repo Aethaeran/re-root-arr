9f8439db4c3f89a255aee46f52b7f83c1133399f
Aethaeran <33813964+Aethaeran@users.noreply.github.com>
2021-07-13 18:41:02 -0700

    Update re-root.py
    Added input prompts to request data from the end user's input rather than have them edit the script.

--- re-root.py
+++ re-root.py
@@ -14,20 +14,47 @@ radarr_db = "radarr.db"
 radarr_root_directory_to_replace = "/your/current/root"
 radarr_root_directory_replacement = "/your/new/root"
 
+sonarr = ""
+while str.lower(sonarr) != "yes" and str.lower(sonarr) != "no":
+    sonarr = input("Do you want to re-root Sonarr?"+"\n")
+    if str.lower(sonarr) != "yes" and str.lower(sonarr) != "no":
+        print("You must answer 'yes' or 'no'.")
+if str.lower(sonarr) == ("yes"):
+    print("Ensure Sonarr is not running until the script is complete.")
+    print("The script needs to know where your sonarr.db is located.")
+    value = input("Please enter the absolute path:\n")
+    print("The script needs to know which root path it is replacing.")
+    sonarr_root_directory_to_replace = input('Please enter the entire root path ie. "/your/current/root":\n')
+    print("The script needs to know which path to replace it with.")
+    sonarr_root_directory_replacement = input('Please enter the entire root path ie. "/your/new/root":\n')
+elif str.lower(sonarr) == ("no"):
+    print("We won't re-root Sonarr then.")
+
+
+radarr = ""
+while str.lower(radarr) != "yes" and str.lower(radarr) != "no":
+    radarr = input("Do you want to re-root Radarr?"+"\n")
+    if str.lower(radarr) != "yes" and str.lower(radarr) != "no":
+        print("You must answer 'yes' or 'no'.")
+if str.lower(radarr) == ("yes"):
+    print("Ensure Radarr is not running until the script is complete.")
+    print("The script needs to know where your radarr.db is located.")
+    value = input("Please enter the absolute path:\n")
+    print("The script needs to know which root path it is replacing.")
+    radarr_root_directory_to_replace = input('Please enter the entire root path ie. "/your/current/root":\n')
+    print("The script needs to know which path to replace it with.")
+    radarr_root_directory_replacement = input('Please enter the entire root path ie. "/your/new/root":\n')
+elif str.lower(radarr) == ("no"):
+    print("We won't re-root Radarr then.")
 
 def sonarr_change_root_directories(your_database, replace_this, with_this):
-    """
-    :param your_database:
-    :param replace_this:
-    :param with_this:
-    :return:
-    """
 
     if os.path.exists(your_database):
         conn = sqlite3.connect(your_database)
         c = conn.cursor()
 
         # Sonarr DB tables that need updating
+        print("Updating sonarr 'RootFolders' table...")
         # RootFolders, Path column
         root_folders_to_change = []
         for row in c.execute('SELECT * FROM RootFolders WHERE Path Like "' + str(replace_this) + '%"'):
@@ -38,6 +65,7 @@ def sonarr_change_root_directories(your_database, replace_this, with_this):
             print(item+" --> "+new_path)
             c.execute('UPDATE RootFolders SET Path="' + new_path + '" WHERE Path like "' + item + '%"')
 
+        print("Updating sonarr 'Series' table...")
         series_to_change = []
         for row in c.execute('SELECT * FROM Series WHERE Path Like "' + str(replace_this) + '%"'):
             series_to_change.append(row[11])  # Append Path column to list
@@ -60,12 +88,6 @@ def sonarr_change_root_directories(your_database, replace_this, with_this):
 
 
 def radarr_change_root_directories(your_database, replace_this, with_this):
-    '''
-    :param your_database:
-    :param replace_this:
-    :param with_this:
-    :return:
-    '''
 
     if os.path.exists(your_database):
         conn = sqlite3.connect(your_database)
@@ -118,7 +140,9 @@ def radarr_change_root_directories(your_database, replace_this, with_this):
 
 
 # Apply changes to sonarr
-sonarr_change_root_directories(sonarr_db, sonarr_root_directory_to_replace, sonarr_root_directory_replacement)
+if str.lower(sonarr) == ("yes"):
+    sonarr_change_root_directories(sonarr_db, sonarr_root_directory_to_replace, sonarr_root_directory_replacement)
 
 # Apply changes to radarr
-radarr_change_root_directories(radarr_db, radarr_root_directory_to_replace, radarr_root_directory_replacement)
+if str.lower(radarr) == ("yes"):
+    radarr_change_root_directories(radarr_db, radarr_root_directory_to_replace, radarr_root_directory_replacement)
